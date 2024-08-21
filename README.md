b1cf014e33c99acc4a4578bada370905d0e04726
Donker_Jumala <weareh0711@gmail.com>
2024-08-21 22:56:50 +0900

    完成

--- README.md
+++ README.md
@@ -1,2 +1,75 @@
 # DoubleBOT
+
 分身の術！！
+
+## About
+
+このBOTは指定したユーザーのステータスに応じてBOTのステータスが変わるBOTです
+</br>
+このBOTにユーザーがオフラインの際でもオフラインであることを分かりやすく表示します
+</br>
+使用方法についてはこの下を読んでください
+
+## Requirement
+
+・node.js `(^20.x.x)`
+</br>
+・discord.js `(14.15.3)`
+</br>
+</br>
+
+## How to use
+
+</br>
+
+まずはこのリポジトリを任意の場所にクローンしてください
+
+```bash
+git clone https://github.com/Jumala9163/DoubleBOT.git
+```
+
+</br>
+
+クローンしたらクローンした先のフォルダーに入っている`config.example.json`をコピーして`config.json`に名前を変更してください
+</br>
+変更後テキストエディタで`config.json`の中身を設定してください
+
+</br>
+
+`config.json`の中身について
+
+```json
+    "DISCORD_BOT_TOKEN" : "ボットのトークン", 
+    "DISCORD_BOT_STATUS_MESSAGE" : "初期状態のボットのステータスメッセージ",
+    "TARGET_GUILD_ID" : "指定したいユーザーが居るサーバーのID", 
+    "TARGET_USER_ID" : "指定したいユーザー自身のID", 
+    "TARGET_USER_OFFLINE_BOT_STATUS_MESSAGE" : "指定したユーザーがオフラインの時のボットのステータスメッセージ",
+    "TARGET_USER_IDLE_BOT_STATUS_MESSAGE" : "指定したユーザーが退席中の時のボットのステータスメッセージ",
+    "TARGET_USER_DND_BOT_STATUS_MESSAGE" : "指定したユーザーが取り込み中の時のボットのステータスメッセージ",
+    "TARGET_USER_ONLINE_BOT_NICK" : "指定したユーザーがオンラインの時のボットのニックネーム",
+    "TARGET_USER_OFFLINE_BOT_NICK" : "指定したユーザーがオフラインの時のボットのニックネーム",
+    "TARGET_USER_IDLE_BOT_NICK" : "指定したユーザーが退席中の時のボットのニックネーム",
+    "TARGET_USER_DND_BOT_NICK" : "指定したユーザーが取り込み中の時のボットのニックネーム"
+```
+
+</br>
+
+次にパッケージのインストールをしてください
+
+```bash
+npm install
+```
+
+</br>
+
+完了したら下記のコマンドを実行すれば動き出すはずです
+
+```bash
+npm start
+```
+
+</br></br>
+
+©2024 [Jumala9163](https://github.com/Jumala9163)
+</br>
+[Released under the Apache-2.0 license](https://github.com/Jumala9163/DoubleBOT/blob/main/LICENSE)
---
+++ app.js
@@ -0,0 +1,92 @@
+// 必要な物を召喚
+const { Client, GatewayIntentBits, ActivityType } = require('discord.js'); 
+const client = new Client({ intents: [GatewayIntentBits.Guilds, GatewayIntentBits.GuildPresences] }); 
+
+// jsonをロード 
+var config = require('./config.json'); 
+
+// TOKEN確認 
+if (config.DISCORD_BOT_TOKEN == undefined) { 
+    console.error("TOKENが設定されていません。"); 
+    process.exit(0); 
+}
+
+// ログイン完了時処理
+client.on("ready", () => { 
+
+    console.log(`ログイン完了: ${client.user.tag}`);
+    console.log(`ターゲットユーザーのステータスを取得します...`)
+
+    const guild = client.guilds.cache.get(config.TARGET_GUILD_ID);
+    const member = guild.members.cache.get(config.TARGET_USER_ID);
+    const botMember = guild.members.cache.get(client.user.id);
+    const target_user_status = member.presence.status;
+
+    console.log(`現在のターゲットユーザーのステータスは ${target_user_status}です`)
+
+    switch (target_user_status) { 
+        case 'online': 
+        client.user.setPresence({ activities: [{ name: `${config.DISCORD_BOT_STATUS_MESSAGE}`, type: ActivityType.Playing }], status: 'invisible', });
+        botMember.setNickname(`${config.TARGET_USER_ONLINE_BOT_NICK}`);
+        console.log("BOTのステータスを切り替えました_OFF");
+        break; 
+        
+        case 'idle': 
+        client.user.setPresence({ activities: [{ name: `${config.DISCORD_BOT_STATUS_MESSAGE}`, type: ActivityType.Playing }], status: 'idle', });
+        botMember.setNickname(`${config.TARGET_USER_IDLE_BOT_NICK}`);
+        console.log("BOTのステータスを切り替えました_IDLE");
+        break; 
+        
+        case 'dnd':
+        client.user.setPresence({ activities: [{ name: `${config.DISCORD_BOT_STATUS_MESSAGE}`, type: ActivityType.Playing }], status: 'dnd', });
+        botMember.setNickname(`${config.TARGET_USER_DND_BOT_NICK}`);
+        console.log("BOTのステータスを切り替えました_DND");
+        break; 
+        
+        case 'offline': 
+        client.user.setPresence({ activities: [{ name: `${config.DISCORD_BOT_STATUS_MESSAGE}`, type: ActivityType.Playing }], status: 'online', });
+        botMember.setNickname(`${config.TARGET_USER_ONLINE_BOT_NICK}`);
+        console.log("BOTのステータスを切り替えました_ON");
+        break; 
+    }
+}); 
+
+
+
+//着火
+client.on('presenceUpdate', (oldPresence, newPresence) => { 
+    const guild = client.guilds.cache.get(config.TARGET_GUILD_ID);
+    const botMember = guild.members.cache.get(client.user.id);
+    const target_user_id = oldPresence.user.id;
+    
+    if(target_user_id == config.TARGET_USER_ID) {
+        switch (newPresence.status) { 
+            case 'online': 
+            console.log(`${oldPresence.user.tag} のステータスがオンラインになりました`); 
+            client.user.setPresence({ activities: [{ name: `${config.DISCORD_BOT_STATUS_MESSAGE}`, type: ActivityType.Playing }], status: 'invisible', });
+            botMember.setNickname(`${config.TARGET_USER_ONLINE_BOT_NICK}`);
+            break; 
+            
+            case 'idle': 
+            console.log(`${oldPresence.user.tag} のステータスが退席中になりました`); 
+            client.user.setPresence({ activities: [{ name: `${config.TARGET_USER_IDLE_BOT_STATUS_MESSAGE}`, type: ActivityType.Playing }], status: 'idle', });
+            botMember.setNickname(`${config.TARGET_USER_IDLE_BOT_NICK}`);
+            break; 
+            
+            case 'dnd': 
+            console.log(`${oldPresence.user.tag} のステータスが取り込み中になりました`); 
+            client.user.setPresence({ activities: [{ name: `${config.TARGET_USER_DND_BOT_STATUS_MESSAGE}`, type: ActivityType.Playing }], status: 'dnd', });
+            botMember.setNickname(`${config.TARGET_USER_DND_BOT_NICK}`);
+            break; 
+            
+            case 'offline': 
+            console.log(`${oldPresence.user.tag} のステータスがオフラインになりました`); 
+            client.user.setPresence({ activities: [{ name: `${config.TARGET_USER_OFFLINE_BOT_STATUS_MESSAGE}`, type: ActivityType.Playing }], status: 'online', });
+            botMember.setNickname(`${config.TARGET_USER_ONLINE_BOT_NICK}`);
+            break; 
+        } 
+    } 
+});
+
+// ログイン
+client.login(config.DISCORD_BOT_TOKEN);
